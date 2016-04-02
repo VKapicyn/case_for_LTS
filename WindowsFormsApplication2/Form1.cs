65330bdeef0e9fa89a04557027102ba4946c346c
VKapicyn <zveronym@gmail.com>
2016-04-02 18:52:16 +0400

    оптимизация итераций

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -29,7 +29,64 @@ namespace WindowsFormsApplication2
         }
 
         private void make_order(object sender, DoWorkEventArgs e)
-        {
+        {          
+            bool flag = true;
+            double stop_los = 0, take_pt = 0, price = 0;
+            int amount = 0;
+
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    if (!textBox3.Text.Equals(""))
+                        price = double.Parse(textBox3.Text);
+                    else
+                        price = 0;
+                }
+                else
+                {
+                    price = double.Parse(textBox3.Text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Некорректное значение \"цена\"\nпопробуйте ввести еще раз.");
+                return;
+            }
+            try
+            {
+                if (!textBox4.Text.Equals(""))
+                    stop_los = double.Parse(textBox4.Text);
+                else
+                    stop_los = 0;
+            }
+            catch
+            {
+                MessageBox.Show("Некорректное значение \"stop-los\"\nпопробуйте ввести еще раз.");
+                return;
+            }
+            try
+            {
+                if (!textBox5.Text.Equals(""))
+                    take_pt = double.Parse(textBox5.Text);
+                else
+                    take_pt = 0;
+            }
+            catch
+            {
+                MessageBox.Show("Некорректное значение \"take-pt\"\nпопробуйте ввести еще раз.");
+                return;
+            }
+            try
+            {
+                amount = int.Parse(textBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Некорректное значение \"количество\"\nпопробуйте ввести еще раз.");
+                return;
+            }
+
             foreach (var security in Security.securities)
             {
                 if (textBox1.Text == "")
@@ -39,87 +96,33 @@ namespace WindowsFormsApplication2
                 }
                 else
                 {
-                    double stop_los = 0, take_pt = 0,price=0;
-
-                    try
-                    {
-                        if (radioButton1.Checked)
-                        {
-                            if (!textBox3.Text.Equals(""))
-                                price = double.Parse(textBox3.Text);
-                            else
-                                price = 0;
-                        }
-                        else
-                        {
-                            price = double.Parse(textBox3.Text);
-                        }
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Некорректное значение \"цена\"\nпопробуйте ввести еще раз.");
-                        break;
-                    }
-                    try
-                    {
-                        if (!textBox4.Text.Equals(""))
-                            stop_los = double.Parse(textBox4.Text);
-                        else
-                            stop_los = 0;
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Некорректное значение \"stop-los\"\nпопробуйте ввести еще раз.");
-                        break;
-                    }
-                    try
-                    {
-                        if (!textBox5.Text.Equals(""))
-                            take_pt = double.Parse(textBox5.Text);
-                        else
-                            take_pt = 0;
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Некорректное значение \"take-pt\"\nпопробуйте ввести еще раз.");
-                        break;
-                    }
-                    int amount = 0;
-                    try
-                    {
-                        amount = int.Parse(textBox2.Text);
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Некорректное значение \"количество\"\nпопробуйте ввести еще раз.");
-                        break;
-                    }
                     if (security.ticker.Equals(textBox1.Text))
                     {
+                        flag = false;
                         if (radioButton1.Checked)
                         {
                             MessageBox.Show(security.AddOrder(amount, stop_los, take_pt));
                         }
                         else
                         {
-                            MessageBox.Show(security.AddOrder(amount, double.Parse(textBox3.Text), stop_los, take_pt));
-                        }
-                        break;
-                    }
-                    else
-                    {
-                        Security new_order = new Security(textBox1.Text);
-                        if (radioButton1.Checked)
-                        {
-                            MessageBox.Show(new_order.AddOrder(amount, stop_los, take_pt));
-                        }
-                        else
-                        {
-                            MessageBox.Show(new_order.AddOrder(amount, price, stop_los, take_pt));
+                            MessageBox.Show(security.AddOrder(amount, price, stop_los, take_pt));
                         }
                         break;
                     }
                 }   
+            }
+
+            if (flag)
+            {
+                Security new_order = new Security(textBox1.Text);
+                if (radioButton1.Checked)
+                {
+                    MessageBox.Show(new_order.AddOrder(amount, stop_los, take_pt));
+                }
+                else
+                {
+                    MessageBox.Show(new_order.AddOrder(amount, price, stop_los, take_pt));
+                }
             }
         }
 
