df8a1d6f1e8b204b93b6477454f059ae4ec0e193
VKapicyn <zveronym@gmail.com>
2016-04-02 12:28:25 +0400

    first comit

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Security.securities = new List<Security>();
+            Security.securities.Add(new Security());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker making_order = new BackgroundWorker();
+            making_order.DoWork += make_order;
+            making_order.RunWorkerAsync();
+        }
+
+        private void make_order(object sender, DoWorkEventArgs e)
+        {
+            foreach (var security in Security.securities)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Вы не заполнили поле \"Инструмент\" заполните\nполе и попробуте еще раз.");
+                    break;
+                }
+                if (security.ticker.Equals(textBox1.Text))
+                {
+                    if (radioButton1.Checked)
+                    {
+                        try
+                        {
+
+                            MessageBox.Show(security.AddOrder(int.Parse(textBox2.Text)));
+                        }
+                        catch 
+                        {
+                            MessageBox.Show("Некорректное значение \"количество\"\nпопробуйте ввести еще раз.");
+                            break;
+                        }   
+                    }
+                    else
+                    {
+                        int amount=0;
+                        try
+                        {
+                            amount = int.Parse(textBox2.Text);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Некорректное значение \"количество\"\nпопробуйте ввести еще раз.");
+                            break;
+                        }
+                        try
+                        {
+                            MessageBox.Show(security.AddOrder(amount, double.Parse(textBox3.Text)));
+                        }
+                        catch 
+                        {
+                            MessageBox.Show("Некорректное значение \"цена\"\nпопробуйте ввести еще раз.");
+                            break;
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    double stop_los = 0, take_pt = 0;
+
+                    try 
+                    {
+                        stop_los = double.Parse(textBox4.Text);
+                    }
+                    catch 
+                    {
+                        MessageBox.Show("Некорректное значение \"stop-los\"\nпопробуйте ввести еще раз.");
+                        break;
+                    }
+                    try 
+                    {
+                        take_pt = double.Parse(textBox5.Text);
+                    }
+                    catch 
+                    {
+                        MessageBox.Show("Некорректное значение \"take-pt\"\nпопробуйте ввести еще раз.");
+                        break;
+                    }
+                    Security new_order = new Security(textBox1.Text,stop_los,take_pt);
+                    if (radioButton1.Checked)
+                    {
+                        try
+                        {
+                            MessageBox.Show(new_order.AddOrder(int.Parse(textBox2.Text)));
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Некорректное значение \"количество\"\nпопробуйте ввести еще раз.");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        int amount = 0;
+                        try
+                        {
+                            amount = int.Parse(textBox2.Text);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Некорректное значение \"количество\"\nпопробуйте ввести еще раз.");
+                            break;
+                        }
+                        try
+                        {
+                            MessageBox.Show(new_order.AddOrder(amount, double.Parse(textBox3.Text)));
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Некорректное значение \"цена\"\nпопробуйте ввести еще раз.");
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ WindowsFormsApplication2/Security.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApplication2
+{
+    class Security
+    {
+        public static List<Security> securities;
+        public Security()
+        {
+            _history = new List<History>();
+            _transactions = new List<int>();
+            this.amount = 0;
+            this.price = 0;
+            this.ticker = "";
+            this.stop_los = 0;
+            this.take_pt = 0;
+        }
+
+        public Security(string ticker, double stop_los,double take_pt)
+        {
+            _history = new List<History>();
+            _transactions = new List<int>();
+            this.ticker = ticker;
+            this.stop_los = stop_los;
+            this.take_pt = take_pt;
+            this.price = 0;
+            this.amount = 0;
+        }
+
+        public static int order=0; 
+        public string ticker 
+            { get; set; }
+
+        public int amount
+            { get; set; }
+
+        public double price 
+            { get; set; }
+
+        public double stop_los
+            { get; set; }
+
+        public double take_pt
+            { get; set; }
+
+        private List<History> _history; 
+
+        public List<History> history
+        {
+            get
+            {
+                return _history;
+            }
+            set 
+            {
+                _history = history;
+            }
+        }
+
+        private List<int> _transactions;
+
+        public List<int> transactions
+        {
+            get 
+            {
+                return _transactions;
+            }
+            set
+            {
+                _transactions = transactions;
+            }
+        }
+
+        public string AddOrder(int amount, double price)
+        {
+            if ((Application.OpenForms[0] as Form1).radioButton4.Checked)
+                this.amount += amount;
+            else
+                this.amount -= amount;
+
+            this.price = price;
+            (Application.OpenForms[0] as Form1).dataGridView1.Rows.Add(order,this.ticker,amount,this.price,"Limit",this.stop_los,this.take_pt);
+            order++;
+
+            return String.Format("Инструмент: {0,-5}\nЦена: {1,-5}\nКоличество: {2,-5}\nТип заявки: {3,-5}\nstop-los: {4,-5}\ntake-pt: {5,-5}",
+                this.ticker, this.price, amount, "Limit", this.stop_los, this.take_pt);
+        }
+
+        public string AddOrder(int amount)
+        {
+            if ((Application.OpenForms[0] as Form1).radioButton4.Checked)
+                this.amount += amount;
+            else
+                this.amount -= amount;
+
+            (Application.OpenForms[0] as Form1).dataGridView1.Rows.Add(order, this.ticker, amount, this.price, "Limit", this.stop_los, this.take_pt);
+            order++;
+            return String.Format("Инструмент: {0,-5}\nКоличество: {2,-5}\nТип заявки: {3,-5}\nstop-los: {4,-5}\ntake-pt: {5,-5}",
+                this.ticker, this.price, amount, "Market", this.stop_los, this.take_pt);
+        }
+
+        public class History
+        {
+            public History(double open, double hight, double low, double close, long volume, DateTime date)
+            {
+                this.open = open;
+                this.hight = hight;
+                this.low = low;
+                this.close = close;
+                this.volume = volume;
+                this.date = date;
+            }
+            public double open
+                { get; set; }
+            public double hight
+                { get; set; }
+            public double low
+                { get; set; }
+            public double close
+                { get; set; }
+            public long volume
+                { get; set; }
+            public DateTime date
+                { get; set; }
+        }
+    }
+}
